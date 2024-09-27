eb68558863bd14c403364973a7febc17bee5c6a6
Josthoon Rivas <josthoonrivas@gmail.com>
2024-09-26 21:58:42 -0300

    comit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!
-        hola mundo
+      <Text> aplicaçao de fornecedores meetting
+        
       </Text>
       <StatusBar style="auto" />
     </View>
