974debc001caf225d056e21e6b7983cf3c8c1b14
Josthoon Rivas <josthoonrivas@gmail.com>
2024-09-28 12:59:13 -0300

    NovoFornecedor component

--- screens/NovoFornecedor.js
+++ screens/NovoFornecedor.js
@@ -1,11 +1,32 @@
-import {Text} from 'react-native'
+import {View,Text, Image, StyleSheet,TextInput, TouchableOpacity, ScrollView} from 'react-native'
 
 const NovoFornecedor = () => {
 
     return(
-        <Text>Novo Fornecedor</Text>
-    )
+       <ScrollView>
+             <Text>Novo Fornecedor</Text>
+             <View>
+                <Text>Novo Fornecedor</Text>
+                <Image
+                source={{
+                    uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+                }}
+                style={{with: 200, height: 200}}
+                />
+             </View>
+            <TextInput
+                style={{
+                    height: 40,
+                    borderColor: 'gray', 
+                    borderWidth: 1,
+                }}
+                defaultValue="Descriçao Fornecedor"
+                />
 
-}
+
+       </ScrollView>
+    );
+
+};
 
 export default NovoFornecedor
