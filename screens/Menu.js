dcee7562b9c8137e93215b6e13f7a06e6efeb79f
Josthoon Rivas <josthoonrivas@gmail.com>
2024-09-29 17:19:25 -0300

    navegacion Menu

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,63 +1,53 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import  MaterialCommunityIcons  from "react-native-vector-icons/MaterialCommunityIcons";
-import { useTheme } from "react-native-paper";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { StyleSheet } from "react-native";
 
-import {ListaFornecedor} from "./ListaFornecedor";
+import ListaFornecedor from "./ListaFornecedor"; // Corrige la importación si es necesario
 import NovoFornecedor from "./NovoFornecedor";
 
 const Tab = createMaterialBottomTabNavigator();
 
 const Menu = () => {
+  return (
+    <Tab.Navigator
+      tabBarActiveBackgroundColor="#fff" // Corregido
+      activeColor="#000" // Corregido
+      inactiveColor="#95a5a6"
+      barStyle={styles.navigationBar}
+    >
+      <Tab.Screen
+        name="Fornecedores"
+        component={ListaFornecedor}
+        options={{
+          tabBarLabel: "Lista de Fornecedores",
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="view-list" color="#000" size={24} />
+          ),
+        }}
+      />
 
-    return(
-        <Tab.Navigator
-            tabBarActivateBackgroundColor = "#fff"
-            activateColor = '#000'
-            inactiveColor = '#95a5a6'
-            barStyle={styles.navigationBar}
-
-            >
-                <Tab.Screen
-                    name="Fornecedores"
-                    component={ListaFornecedor}
-                    options={{
-                        tabBarLabel: "Lista de Fornecedores",
-                        tabBarIcon: () => (
-                            <MaterialCommunityIcons name="view-list" color="#000" size={24} />
-                        )
-                    }}
-                >
-
-                </Tab.Screen>
-                
-                <Tab.Screen
-                    name="Novo Fornecedor"
-                    component={NovoFornecedor}
-                    options={{
-                        tabBarLabel: "Novo Fornecedor",
-                        tabBarIcon: () => (
-                            <MaterialCommunityIcons name="checkbox-marked-circle-plus-outline" color="#000" size={24} />
-                        )
-                    }}
-                >
-                    
-                </Tab.Screen>
-
-
-
-
-            </Tab.Navigator>
-    )
-
-}
-
+      <Tab.Screen
+        name="Novo Fornecedor"
+        component={NovoFornecedor}
+        options={{
+          tabBarLabel: "Novo Fornecedor",
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="checkbox-marked-circle-plus-outline"
+              color="#000"
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
-    navigationBar: {
-        backgroundColor:"#fff"
-    }
-})
-
+  navigationBar: {
+    backgroundColor: "#fff",
+  },
+});
 
 export default Menu;
